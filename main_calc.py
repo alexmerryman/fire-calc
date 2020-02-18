7bbc0a1bb364e6a1cae67f126e45bb35db1ffbe1
Alexander Merryman <merryman.alexander@gmail.com>
2020-02-18 13:38:02 -0600

    Initial commit

---
+++ main_calc.py
@@ -0,0 +1,29 @@
+
+
+import pandas as pd
+
+
+# input params
+salary_gross
+salary_growth
+tax_now
+SWR
+annual_spend
+tax_retirement
+market_return
+inflation
+
+# assets
+bank_accts (checking, saving)
+savings_APY
+retirement/investment/brokerage accts
+contrib
+home_equity ?
+car_value ?
+
+# liabilities
+student_loans (principal and APY)
+mortgage
+car_loan
+
+
